1c11525671169fc570665553abb4578485132080
chaitramk <cmk6790@gmail.com>
2025-04-25 15:55:51 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, Chaitra! Your Docker setup is working!";
+        return "Hello, raksha! Your Docker setup is working!";
     }
 }
